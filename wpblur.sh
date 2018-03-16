5da5fd870c458c50516c651134bca44528995e45
Kenneth So <kennethso168@gmail.com>
2018-03-16 22:20:15 +0800

    Also detect change of Activity

--- wpblur.sh
+++ wpblur.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
+function blur {
+    echo "Blurring the background"
+    sleep 2
+    qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'd = desktopForScreen(0); d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General"); print("cw=" + d.readConfig("Image"));'
+    CURRENT_WP_PATH=$(journalctl -n 10 | grep -o 'cw=.*' | tail -n 1 | sed -E 's/cw=(file:\/\/)?//;s/"$//')
+    convert "$CURRENT_WP_PATH" -filter Gaussian -resize 5% -define filter:sigma=2.5 -resize 2000% -attenuate 0.2 +noise Gaussian ~/.bg.png
+    echo "Background blurring finished"
+}
+
 if [ $(pidof -x wpblur.sh| wc -w) -gt 2 ]; then
 	echo wpblur already running, exiting
     exit 1
 fi
 
 while true; do
-    inotifywait -q ~/.config/plasma-org.kde.plasma.desktop-appletsrc -e delete_self -e open | while read; do
-        sleep 2
-        qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'd = desktopForScreen(0); d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General"); print("cw=" + d.readConfig("Image"));'
-        CURRENT_WP_PATH=$(journalctl -n 10 | grep -o 'cw=.*' | tail -n 1 | sed -E 's/cw=(file:\/\/)?//;s/"$//')
-        convert "$CURRENT_WP_PATH" -filter Gaussian -resize 5% -define filter:sigma=2.5 -resize 2000% -attenuate 0.2 +noise Gaussian ~/.bg.png
+    inotifywait -q ~/.config/plasma-org.kde.plasma.desktop-appletsrc -e delete_self | while read; do
+        echo "~/.config/plasma-org.kde.plasma.desktop-appletsrc modified"
+        blur
     done
-done
+done &
+
+interface=org.kde.ActivityManager.Activities
+member=CurrentActivityChanged
+
+dbus-monitor --profile "interface='$interface',member='$member'" |
+while read -r line; do
+    if [[ $line = *"CurrentActivityChanged"* ]]; then
+        echo "Activity changed"
+        blur
+    fi
+done &
+
+sleep infinity
