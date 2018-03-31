2a64b8246ba50174f7f1067c5fc7a0769f15a6d7
wsdfhjxc <wsdfhjxc@protonmail.com>
2018-04-01 01:32:50 +0200

    Split the blurring function into smaller parts

--- wpblur.sh
+++ wpblur.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-function blur {
-    echo "Blurring the background"
-    sleep 2
+function currentWpPath {
     curActivityId=$(qdbus org.kde.ActivityManager /ActivityManager/Activities CurrentActivity)
     while read containmentId; do 
         lastDesktop=$(kreadconfig5 --file ~/.config/plasma-org.kde.plasma.desktop-appletsrc --group Containments --group $containmentId --key lastScreen)
@@ -11,8 +9,19 @@ function blur {
             CURRENT_WP_PATH=$(kreadconfig5 --file ~/.config/plasma-org.kde.plasma.desktop-appletsrc --group Containments --group $containmentId --group Wallpaper --group org.kde.image --group General --key Image | sed -E 's/(file:\/\/)?//')
         fi
     done <<< "$(grep -e '\[Containments]\[[0-9]*]\[Wallpaper]\[org.kde.image]\[General]' ~/.config/plasma-org.kde.plasma.desktop-appletsrc | sed 's/\[Containments\]\[//;s/]\[Wallpaper]\[org.kde.image]\[General]//')" 
+    echo "$CURRENT_WP_PATH"
+}
+
+function blurWp {
+    CURRENT_WP_PATH="$1"
+    echo "Blurring the background"
     convert "$CURRENT_WP_PATH" -filter Gaussian -resize 5% -define filter:sigma=2.5 -resize 2000% -attenuate 0.2 +noise Gaussian ~/.bg.png
     echo "Background blurring finished"
+}
+
+function blur {
+    CURRENT_WP_PATH=$(currentWpPath)
+    blurWp "$CURRENT_WP_PATH"
 }
 
 if [ $(pidof -x wpblur.sh| wc -w) -gt 2 ]; then
